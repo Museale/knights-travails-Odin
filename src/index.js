6f06e6d7b9fbb1575370c9ea746950b75af7e27d
Elat <52134994+Museale@users.noreply.github.com>
2023-10-20 12:25:53 +0000

    traverse func complete

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import { prettyPrint } from "../_prettyprint";
 const createGameboard = () => {
   const gameboard = [];
 
@@ -21,11 +20,6 @@ const createNode = (position) => {
 };
 
 const knightMoves = (start, goal) => {
-  //rewrite func using graph datastructure
-
-  //create node from gameboard arr
-  // give each node edges (up to 8  possible moves)
-  // implement bfs
 
   const graph = {};
 
@@ -40,119 +34,66 @@ const knightMoves = (start, goal) => {
     [2, 1],
   ];
 
-  const addEdge = (graph, node, moves) => {
-    if(!graph[node]){
-    graph[node] = [];
-  }
-    graph[node].push(...moves);
+  const nodeToString = (node) => {
+    return [node.x, node.y];
   };
 
-  function nodeToString(node) {
-    return `${node.x},${node.y}`;
-  }
-
   const gameboard = createGameboard();
-
+  //iterate through array
   gameboard.forEach((element) => {
     const node = createNode(element);
     const validMoves = [];
-
+    //calculate valid moves for each square
     for (const [dx, dy] of possibleMoves) {
       const newX = node.x + dx;
       const newY = node.y + dy;
 
       if (newX >= 0 && newY >= 0 && newX < 8 && newY < 8) {
-        validMoves.push([newX, newY])
+        validMoves.push([newX, newY]);
       }
     }
-
-    return graph[nodeToString(node)] = validMoves;
-  
+    //add edges to each node
+    return (graph[nodeToString(node)] = validMoves);
   });
 
-  console.log(graph);
+  const traverse = (start, goal) => {
+    //BFS search alg
+    //check if node is visited & queue !empty
+    const visited = {};
+    const queue = [start];
+    const parent = {};
 
-  const startIndex = gameboard.findIndex(
-    (pos) => pos[0] === start[0] && pos[1] === start[1],
-  );
+    visited[start] = true;
 
-  // start = createNode(gameboard[startIndex]);
-  // console.log(start)
-
-  // const createTree = (start) => {
-  //   const moves = (node) => {
-  //     const surroundingPositions = [];
-
-  //   const buildTree = (array, startOfArr = 0, end = array.length - 1) => {
-  //     if (startOfArr > end) return null;
-
-  //     const mid = Math.floor((startOfArr + end) / 2);
-  //     const root = createNode(array[mid]);
-
-  //     root.around = moves(root);
-
-  //     root.left = buildTree(array, startOfArr, mid - 1);
-  //     root.right = buildTree(array, mid + 1, end);
-
-  //     return root;
-  //   };
-
-  //   const board = buildTree(gameboard);
-
-  //   // const findNode = (nodeToBeFound, node) => {
-  //   //   if (node === null) {
-  //   //     console.log('Node not present in tree.');
-  //   //     return false;
-  //   //   }
-  //   //   let found = false;
-  //   //   //If values are equal return true
-  //   //   console.log(nodeToBeFound, node);
-  //   //   if (nodeToBeFound == node.data) {
-  //   //     console.log(`Node with value ${node.data} is present in tree: `, node);
-  //   //     found = true;
-  //   //   }
-  //   //   //traverse recursively through tree
-  //   //   if (nodeToBeFound > node.data) {
-  //   //     found = findNode(nodeToBeFound, node.right);
-  //   //   } else if (nodeToBeFound < node.data) {
-  //   //     found = findNode(nodeToBeFound, node.left);
-  //   //   }
-  //   //   return found;
-  //   // };
-  //   // console.log(goal);
-  //   // console.log(findNode(goal, board));
-
-  //   const traverse = (node, goal) => {
-  //     if (node === null) return;
-  //     node.around.forEach(el => {
-  //      if (el == goal) {
-  //       const path = [start, goal];
-  //       console.log('Path taken', path)
-  //       return path;
-  //      }
-
-  //     })
-  //     traverse(node.left);
-  //     traverse(node.right);
-  //   };
-
-  //   console.log(start, 'start');
-  //   prettyPrint(board);
-
-  //   const findPath = (node, goal) => {
-
-  //     traverse(node, goal);
-
-  //     //make an array of  [start, move, move, move, move, move, move, move, move];
-  //     //all arrays start with start + first move
-  //     //
-  //   };
-  //   findPath(board, goal)
-  //   return board;
-  // };
-
-  // const tree = createTree(gameboard);
-
-  // rewrite
+    while (queue.length) {
+      //dequeue currvertx 
+      const currentVertex = queue.shift();
+      //check if goal === currvertx
+      if (currentVertex[0] === goal[0] && currentVertex[1] === goal[1]) {
+        //if goal is reached, construct shortest path
+        const path = [currentVertex];
+        let node = parent[currentVertex];
+        //traverse paarent pointers to build path
+        while (node) {
+          path.unshift(node);
+          node = parent[node];
+        }
+        console.log("Shortest path:", path);
+        return;
+      }
+      //iterate through neighbouors of currvertx
+      for (const neighbour of graph[currentVertex]) {
+        //if !visited
+        if (!visited[neighbour]) {
+          visited[neighbour] = true;
+          //update parent of neighbour to currvertx
+          parent[neighbour] = currentVertex;
+          queue.push(neighbour);
+        }
+      }
+    }
+  };
+  traverse(start, goal);
 };
-knightMoves([0, 0], [1, 2]);
+
+knightMoves([3, 3], [4, 3]);
